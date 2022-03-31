9088c1e169764c9aa8c81b1537a31a382a0f6a12
DiegoBelPe <dbeltranpereira@gmail.com>
2022-03-30 22:13:06 -0500

    create petition

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const { json } = require("express/lib/response");
 const morgan = require("morgan");
 
 const mongoose = require("mongoose");
-const configExpress=require('./config/express')
-const connectDB= require('./config/database')
+const configExpress = require("./config/express");
+const connectDB = require("./config/database");
 
 const app = express();
 
 configExpress(app);
 
-connectDB()
+connectDB();
 
 const url = process.env.MONGO_DB_URI;
 /* mongoose.connect(url);
@@ -49,38 +49,79 @@ app.use(morgan(":method :url :status :body - :response-time ms"));
 const tareas = [
   {
     id: 1,
-    name: "sociales",
-    number: "040-123456",
+    course: "Programacion",
+    description: "react-mongo",
+    observations: "enviar info",
+    endDate: "22 / 03 / 2022",
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "29-44-5323523",
+    course: "Español",
+    description: "react-mongo",
+    observations: "enviar info",
+    endDate: "22 / 03 / 2022",
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6424122",
-  },
-  {
-    id: 5,
-    name: "Emerson Asto",
-    number: "40-232-578456",
+    course: "Ingles",
+    description: "react-mongo",
+    observations: "enviar info",
+    endDate: "22 / 03 / 2022",
   },
 ];
+
 app.get("/api/tareas", (req, res) => {
   res.json(tareas);
 });
+app.get("/api/tareas/:id", (req, res) => {
+  const id = req.params.id;
+  const tarea = tareas.find((tarea) => tarea.id === Number(id));
+
+  if (!tarea) {
+    res.status(404).json({ message: "tarea no encontrada" });
+  } else {
+    res.json(tarea);
+  }
+});
+app.post("/api/tareas", (req, res) => {
+  const tarea = {
+    ...req.body,
+    id: tareas.length + 1,
+  };
+  if (!tarea.course) {
+    res.status(400).json({ message: "La tarea es requerida" });
+  }
+
+  if (!tarea.description) {
+    res.status(400).json({ message: "Descripcion es requerida" });
+  } else {
+    res.status(201).json({ Message: "Tarea agregada!" });
+  }
+
+  tareas.push(tarea);
+  console.log("tareas", tarea.course);
+  return res.json(tareas);
+});
+app.patch("/api/tareas/:id", (req, res) => {
+  const id = req.params.id;
+  const { body } = req;
+
+  tareas.forEach((tarea) => {
+    if (tarea.id === Number(id)) {
+      tarea.course = body.course;
+      tarea.description = body.description;
+      tarea.observations = body.observations;
+      tarea.endDate = body.endDate;
+    }
+  });
+  res.json(body);
+});
+/*
 const getCurrentDate = () => {
   const today = new Date();
   return today;
 };
-app.get("/info", (req, res) => {
+app.get("/info", (req, res) => { 
   res.send(`<p> Phonebbok has info for ${tareas.length} people </p> \n 
             ${getCurrentDate()}`);
 });
@@ -107,7 +148,6 @@ app.delete("/api/tareas/:id", (req, res) => {
   }
 });
 
-//1.5: backend de la agenda telefónica
 app.post("/api/tareas", (req, res) => {
   const id = Math.round(Math.random() * 10000);
   const nuevaPersona = { ...req.body, id };
@@ -115,7 +155,6 @@ app.post("/api/tareas", (req, res) => {
   res.status(201).json({ Message: "Contacto agregado!" });
 });
 
-//1.6: backend de la agenda telefónica
 
 app.post("/api/tareas", (req, res) => {
   const id = Math.round(Math.random() * 10000);
@@ -138,7 +177,7 @@ app.post("/api/tareas", (req, res) => {
     res.status(201).json({ Message: "Contacto agregado!" });
   }
 });
-
+ */
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
