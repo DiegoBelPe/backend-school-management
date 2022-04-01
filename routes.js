5ddce056bb5dbadbd686345290f46f7f15a2d7d0
DiegoBelPe <dbeltranpereira@gmail.com>
2022-04-01 12:28:18 -0500

    update homerWork ok

--- routes.js
+++ routes.js
@@ -1,21 +1,21 @@
 /**
  * Main application routes
  */
-const healthcheck = require('./api/healthcheck');
-const task = require('./api/task');
-const user = require('./api/user');
-const authLocal = require('./auth/local');
+const healthcheck = require("./api/healthcheck");
+const task = require("./api/task");
+const user = require("./api/user");
+const authLocal = require("./auth/local");
 const homework = require("./api/homerWork");
 
 function routes(app) {
   // API Routes
-  app.use('/api/healthcheck', healthcheck);
-  app.use('/api/tasks', task);
-  app.use('/api/users', user);
+  app.use("/api/healthcheck", healthcheck);
+  app.use("/api/tasks", task);
+  app.use("/api/users", user);
   app.use("/api/tareas", homework);
 
   // auth Routes
-  app.use('/auth/local', authLocal);
+  app.use("/auth/local", authLocal);
   // app.use('/auth/facebook', facebookLocal);
   // app.use('/auth/google', googleLocal);
 }
