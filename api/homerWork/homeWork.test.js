9e643aded3983ee2827e5275c0b5a53f552333f7
DiegoBelPe <dbeltranpereira@gmail.com>
2022-04-11 11:42:23 -0500

    test endpoints get/tareas:id

--- api/homerWork/homeWork.test.js
+++ api/homerWork/homeWork.test.js
@@ -5,12 +5,31 @@ const app = require('../../app');
 const request = supertest(app);
 
 describe('task Endpoints', () => {
-  describe('GET/tareas', ()=>{
+  describe('GET/tareas', () => {
     test('should return 200 OK', async () => {
       const response = await request.get('/api/tareas');
       expect(response.statusCode).toEqual(200);
     });
-
-  })
+    test('should return an array', async () => {
+      const response = await request.get('/api/tareas');
+      expect(response.body).toBeInstanceOf(Array);
+    });
+  });
+  describe('GET/tareas/:id', () => {
+    test('should return 200 OK', async () => {
+      const id = '6246391d6915ca88d60f988c';
+      const response = await request.get(`/api/tareas/${id}`);
+      expect(response.statusCode).toEqual(200);
+    });
+    test('should respond with an array of user and with date specifics GET/:id', async () => {
+      const id = '6246391d6915ca88d60f988c';
+      const res = await request.get(`/api/tareas/${id}`);
+      expect(res.body).toEqual(expect.objectContaining({
+        course: expect.any(String),
+        description: expect.any(String),
+        observation: expect.any(String),
+        endDate: expect.any(String),
+      }));
+    });
+  });
 });
-
